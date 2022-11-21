2f9a009034c928a0aae0b8b85ba502f9a03da737
francesco.fum <francesco.fum@gmail.com>
2022-11-21 22:02:11 +0000

    enums

---
+++ cpp_course_main/enum/Status.h
@@ -0,0 +1,20 @@
+/* Old C style of enums */
+enum Status
+{
+    Pending,
+    Approved, 
+    Cancelled
+};
+
+/* Newer class enums */
+enum class FileError
+{
+    notfound, 
+    ok 
+};
+/* Newer class enums */
+enum class NetworkError
+{
+    disconnected, 
+    ok 
+};
---
+++ cpp_course_main/enum/main.cpp
@@ -0,0 +1,13 @@
+#include "Status.h"
+
+
+int main()
+{
+    Status s = Pending; 
+    s = Approved; 
+
+    FileError fe = FileError::notfound; 
+    fe = FileError::ok; 
+    NetworkError ne = NetworkError::disconnected;
+    ne = NetworkError::ok;
+}
